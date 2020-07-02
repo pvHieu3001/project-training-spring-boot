5fd84332103a64d1a287508d5a6d470163197cb6
namcx12 <namcx12@gmail.com>
2020-07-02 17:11:54 +0700

    config i18n

--- Java_Project/service/src/main/java/com/smartosc/training/config/WebConfig.java
+++ Java_Project/service/src/main/java/com/smartosc/training/config/WebConfig.java
@@ -1,5 +1,19 @@
 package com.smartosc.training.config;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 /**
  * Fresher-Training
  *
@@ -8,6 +22,30 @@ package com.smartosc.training.config;
  * @created_by Namtt
  * @since 02/07/2020
  */
-public class WebConfig {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+  @Bean(name = "localeResolver")
+  public LocaleResolver getLocaleResolver() {
+    CookieLocaleResolver resolver = new CookieLocaleResolver();
+    resolver.setCookieDomain("myAppLocaleCookie");
+//    resolver.setDefaultLocale(Locale.ENGLISH);
+    // 60 minutes
+    resolver.setCookieMaxAge(60 * 60);
+    return resolver;
+
+  }
+
+  @Bean(name = "messageSource")
+  public MessageSource getMessageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    //Read file i18n/messages_XXX.properties
+    messageSource.setBasename("classpath:i18n/messages");
+    messageSource.setAlwaysUseMessageFormat(false);
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+
 
 }
