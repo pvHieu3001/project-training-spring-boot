6167f89a1ec7d3d5a14c40d49b42dbceb16b9cdb
Thanh031298 <tranthanh.031298@gmail.com>
2020-07-06 17:56:04 +0700

    modified: update cực mạnh

--- Java_Project/service/src/main/java/com/smartosc/training/services/impl/UserServicesImpl.java
+++ Java_Project/service/src/main/java/com/smartosc/training/services/impl/UserServicesImpl.java
@@ -1,5 +1,8 @@
 package com.smartosc.training.services.impl;
 
+import com.smartosc.training.dto.CommentDTO;
+import com.smartosc.training.dto.RoleDTO;
+import com.smartosc.training.dto.StatusOTDTO;
 import com.smartosc.training.dto.UserDTO;
 import com.smartosc.training.entities.Role;
 import com.smartosc.training.entities.User;
@@ -73,11 +76,30 @@ public class UserServicesImpl implements UserService {
     @Override
     public UserDTO findUserByUserName(String name) throws NotFoundException {
 
-        Optional<User> userEntity = userRepository.findByUsername11(name);
-        if (userEntity.isPresent()) {
-            return modelMapper.map(userEntity.get(), UserDTO.class);
+        User userEntity = userRepository.findByUsername(name);
+        if (userEntity != null) {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setId(userEntity.getId());
+            userDTO.setUsername(userEntity.getUsername());
+            userDTO.setEmail(userEntity.getEmail());
+            userDTO.setPassword(userEntity.getPassword());
+            userDTO.setStatus(userEntity.getStatus());
+            List<Role> roleList =  userEntity.getRoles();
+            List<RoleDTO> roleDTOS = new ArrayList<>();
+            for (Role role: roleList){
+                RoleDTO roleDTO = new RoleDTO();
+                roleDTO.setId(role.getRoleId());
+                roleDTO.setName(role.getName());
+                roleDTOS.add(roleDTO);
+            }
+            userDTO.setRoles(roleDTOS);
+            List<CommentDTO> commentDTOS = new ArrayList<>();
+            userDTO.setComments(commentDTOS);
+            StatusOTDTO statusOTDTOS = new StatusOTDTO();
+            userDTO.setStatusOT(statusOTDTOS);
+            return  userDTO;
         } else {
-            throw new NotFoundException("UnAuthorized");
+            throw new NotFoundException("User " + name + "Not Found");
         }
     }
 
