4e9d1519951aa19155681ed71e2640081bb299dc
lamlt <lamlt@smartosc.com>
2020-07-07 17:29:28 +0700

    bai bai anh Nam

--- Java_Project/service/src/main/java/com/smartosc/training/services/TypeRoomService.java
+++ Java_Project/service/src/main/java/com/smartosc/training/services/TypeRoomService.java
@@ -10,16 +10,18 @@ import java.util.List;
 /**
  * Fresher-Training
  *
-
  * @author Hieupv
  * @created_at 02/07/2020 - 1:59 PM
  * @created_by Hieupv
  * @since 02/07/2020
  */
 public interface TypeRoomService {
-    public List<TypeRoomDTO> findTypeRoomById(Long id);
-    public TypeRoomDTO updateTypeRoom(TypeRoomDTO typeRoomRequest) throws NotFoundException;
-    public TypeRoomDTO createTypeRoom(TypeRoomDTO typeRoomRequest);
-    public void deleteTypeRoom(Long[] id);
+    List<TypeRoomDTO> findTypeRoomById(Long id);
+
+    TypeRoomDTO updateTypeRoom(TypeRoomDTO typeRoomRequest) throws NotFoundException;
+
+    TypeRoomDTO createTypeRoom(TypeRoomDTO typeRoomRequest);
+
+    void deleteTypeRoom(Long[] id);
 
 }
