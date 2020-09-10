a5ebd460d9b73be6df5d6f69d9be3ef36e727782
pvHieu3001 <phamvanhieu300198@gmail.com>
2020-09-10 18:00:01 +0700

    hah

--- Java_Project/authorizationserver/src/main/java/com/smartosc/training/controllers/UserController.java
+++ Java_Project/authorizationserver/src/main/java/com/smartosc/training/controllers/UserController.java
@@ -7,12 +7,14 @@ import com.smartosc.training.services.UserService;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 @RequestMapping(value = "/users")
 public class UserController {
   
@@ -20,13 +22,20 @@ public class UserController {
   private UserService userService;
   
   @GetMapping("/authenticate")
+  @ResponseBody
   public ResponseEntity<Principal> user(Principal user) {
    return ResponseEntity.<Principal>ok(user);
   }
   
   @GetMapping
+  @ResponseBody
   public ResponseEntity<UserDTO> getUserByUsername(Principal principal) throws NotFoundException {
     UserDTO user = userService.findUserByUserName(principal.getName());
     return ResponseEntity.ok(user);
+  }
+
+  @GetMapping("/demo")
+  public String demo(){
+    return "demo";
   }
 }
--- Java_Project/basecore/src/main/java/com/smartosc/training/thread/HelloMain.java
+++ Java_Project/basecore/src/main/java/com/smartosc/training/thread/HelloMain.java
@@ -1,7 +1,21 @@
 package com.smartosc.training.thread;
 
 public class HelloMain {
+    public static class Thread1 extends Thread {
+        public void run() {
+            System.out.println("A");System.out.println("B");
+        }
+    }
+    public static class Thread2 extends Thread {
+        public void run() {
+            System.out.println("1");
+            System.out.println("2");
+        }
+    }
     public static void main(String[] args) throws InterruptedException {
+
+        new Thread1().start();
+        new Thread2().start();
 
         int idx = 1;
 
